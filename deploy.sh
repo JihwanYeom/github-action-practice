b150534306fce8688b9443372894a58705a112d8
jihwanyeom <yum020428@gmail.com>
2025-08-20 14:04:50 +0900

    chore : 권한 추가

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ chmod +x ./gradlew
 echo "--- Terminating existing process if any ---"
 # pkill: pgrep(찾기)과 kill(죽이기)을 한 번에 수행합니다.
 # || true: 종료할 프로세스가 없어도 오류로 간주하지 않습니다.
-pkill -f "java -jar.*github-action-practice.*\.jar" || true
+sudo pkill -f "java -jar.*github-action-practice.*\.jar" || true
 echo "Waiting 5 seconds for shutdown..."
 sleep 5
 # --- ✨ 수정 끝 ✨ ---
